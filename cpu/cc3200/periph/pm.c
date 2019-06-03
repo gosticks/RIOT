14531170d1bc1d2132a4eade1b690e60c8f30774
Wlad Meixner <wlad.meixner@protonmail.com>
2019-06-03 13:25:55 +0200

    feat: add experimental hibernate mode

--- cpu/cc3200/periph/pm.c
+++ cpu/cc3200/periph/pm.c
@@ -9,9 +9,30 @@
 #include "driverlib/rom_map.h"
 #include "periph/pm.h"
 
+#ifdef PROVIDES_PM_SET_LOWEST_CORTEXM
+// TODO: needs to be impemented 
+void pm_set_lowest(void)
+{
+    /* this will hibernate with no way to wake up for now */
+    
+    // write to the hibernate register
+    HWREG(HIB3P3_BASE+HIB3P3_O_MEM_HIB_REQ) = 0x1;
+
+    // wait for 200 uSec 
+    UtilsDelay((80*200)/3);
+}
+#endif
 
 void pm_off(void)
 {
     /* No Generic Power off Mechanism */
-    // MAP_PRCMMCUReset();
+}
+
+// wait based on 3 cycles of the CPU
+void __attribute__((naked))
+delay(unsigned long ulCount)
+{
+    __asm("    subs    r0, #1\n"
+          "    bne     delay\n"
+          "    bx      lr");
 }
