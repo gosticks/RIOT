3c5c2fe637e62544dcd27219a4d575216ce5c443
Wlad Meixner <wlad.meixner@protonmail.com>
2019-06-17 16:17:15 +0200

    cc3200: clang format cleanup

--- boards/cc3200-launchxl/board.c
+++ boards/cc3200-launchxl/board.c
@@ -23,19 +23,18 @@
 // #include "driverlib/interrupt.h"
 // #include "driverlib/utils.h"
 
-#include "cpu.h"
 #include "board.h"
-#include "vendor/hw_types.h"
-#include "vendor/hw_memmap.h"
-#include "vendor/hw_gpio.h"
-#include "periph/gpio.h"
+#include "cpu.h"
+#include "driverlib/gpio.h"
 #include "driverlib/pin.h"
+#include "driverlib/prcm.h"
 #include "driverlib/rom.h"
 #include "driverlib/rom_map.h"
-#include "driverlib/gpio.h"
 #include "driverlib/utils.h"
-#include "driverlib/prcm.h"
-
+#include "periph/gpio.h"
+#include "vendor/hw_gpio.h"
+#include "vendor/hw_memmap.h"
+#include "vendor/hw_types.h"
 
 // #include "periph/gpio.h"
 extern const void cortex_vector_base;
@@ -43,35 +42,32 @@ extern const void cortex_vector_base;
 /**
  * @brief Initialize on-board LEDs
  */
-void led_init(void)
-{
-     //
-    // Enable Peripheral Clocks 
-    //
-    MAP_PRCMPeripheralClkEnable(PRCM_GPIOA1, PRCM_RUN_MODE_CLK);
+void led_init(void) {
+  //
+  // Enable Peripheral Clocks
+  //
+  MAP_PRCMPeripheralClkEnable(PRCM_GPIOA1, PRCM_RUN_MODE_CLK);
 
-    // enable leads and clear them
-    gpio_init(LED_RED, GPIO_OUT);
-    gpio_clear(LED_RED);
+  // enable leads and clear them
+  gpio_init(LED_RED, GPIO_OUT);
+  gpio_clear(LED_RED);
 
-    gpio_init(LED_ORANGE, GPIO_OUT);
-    gpio_clear(LED_ORANGE);
-    
-    gpio_init(LED_GREEN, GPIO_OUT);
-    gpio_clear(LED_GREEN);
+  gpio_init(LED_ORANGE, GPIO_OUT);
+  gpio_clear(LED_ORANGE);
+
+  gpio_init(LED_GREEN, GPIO_OUT);
+  gpio_clear(LED_GREEN);
 }
-
 
 /**
  * @brief Initialize the board
  */
-void board_init(void)
-{
-    /* initialize the CPU */
-    cpu_init();
+void board_init(void) {
+  /* initialize the CPU */
+  cpu_init();
 
-    /* initialize the boards LEDs */
-    led_init();
+  /* initialize the boards LEDs */
+  led_init();
 }
 
 /** @} */
--- cpu/cc3200/include/periph_cpu.h
+++ cpu/cc3200/include/periph_cpu.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Ludwig Maximilian Universität 
+ * Copyright (C) 2019 Ludwig Maximilian Universität
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -11,20 +11,30 @@
  * @{
  *
  * @file
- * @brief           CPU specific definitions and functions for peripheral handling
+ * @brief           CPU specific definitions and functions for peripheral
+ * handling
  *
  * @author          Wladislaw Meixner <wladislaw.meixner@campus.lmu.de>
  */
 
 #include "cc3200.h"
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #ifndef PERIPH_CPU_H
 #define PERIPH_CPU_H
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
+
+#define TIMER_NUMOF (4U)
+
+#define T0 TIMER_DEV(0U)
+#define T1 TIMER_DEV(1U)
+#define T2 TIMER_DEV(2U)
+#define T3 TIMER_DEV(3U)
 
 /**
  * @name    Define a custom type for GPIO pins
@@ -43,23 +53,21 @@ typedef uint32_t gpio_t;
 #define PROVIDES_PM_OFF
 /** @} */
 
-
 /**
  * @brief   Define a CPU specific GPIO pin generator macro
  */
-#define GPIO_PIN(x, y)          ((x << 6) | y)
+#define GPIO_PIN(x, y) ((x << 6) | y)
 #define GPIO_PINS_PER_PORT 8
-#define LED_RED     GPIO_PIN(1, 9)
-#define LED_ORANGE  GPIO_PIN(1, 10)
-#define LED_GREEN   GPIO_PIN(1, 11)
-
+#define LED_RED GPIO_PIN(1, 9)
+#define LED_ORANGE GPIO_PIN(1, 10)
+#define LED_GREEN GPIO_PIN(1, 11)
 
 typedef struct {
-    unsigned long busaddr;            /**< bus address */
-    gpio_t mosi_pin;        /**< pin used for MOSI */
-    gpio_t miso_pin;        /**< pin used for MISO */
-    gpio_t sck_pin;         /**< pin used for SCK */
-    gpio_t cs_pin;          /**< pin used for CS */
+  unsigned long busaddr; /**< bus address */
+  gpio_t mosi_pin;       /**< pin used for MOSI */
+  gpio_t miso_pin;       /**< pin used for MISO */
+  gpio_t sck_pin;        /**< pin used for SCK */
+  gpio_t cs_pin;         /**< pin used for CS */
 } spi_conf_t;
 
 // /**
@@ -83,7 +91,7 @@ typedef struct {
 /**
  * @brief   Length of the CPU_ID in octets
  */
-#define CPUID_LEN           (4U)
+#define CPUID_LEN (4U)
 /** @} */
 
 #ifdef __cplusplus
