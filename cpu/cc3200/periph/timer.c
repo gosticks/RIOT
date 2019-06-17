16d05c86458485d365694ddcee0175cdd11c6648
Wlad Meixner <wlad.meixner@protonmail.com>
2019-06-17 16:16:43 +0200

    cc3200: timer rewrite

--- cpu/cc3200/periph/timer.c
+++ cpu/cc3200/periph/timer.c
@@ -1,406 +1,255 @@
 /*
- * Copyright (C) 2015 Attilio Dona'
+ * Copyright (C) 2019 Ludwig Maximilian Universität
  *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License v2.1. See the file LICENSE in the top level directory for more
- * details.
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
  */
 
 /**
- * @ingroup     cpu_cc3200
+ * @defgroup        cpu_cc3200
  * @{
  *
  * @file
- * @brief       Low-level timer driver implementation
+ * @brief           Timer implementation
+ * handling
  *
- * @author      Attilio Dona'
- *
- * @}
+ * @author          Wladislaw Meixner <wladislaw.meixner@campus.lmu.de>
  */
-
 #include <stdlib.h>
 
-#include <thread.h>
 #include "panic.h"
 #include <sys/types.h>
+#include <thread.h>
 
-#include "periph_conf.h"
 #include "periph/timer.h"
+#include "periph_conf.h"
+#include "xtimer.h"
 
-#include "driverlib/timer.h"
 #include "driverlib/prcm.h"
+#include "driverlib/timer.h"
 
+#include "vendor/hw_ints.h"
 #include "vendor/hw_memmap.h"
 #include "vendor/hw_timer.h"
 #include "vendor/hw_types.h"
 
-#ifndef UNUSED
-#define UNUSED(x) ((x) = (x))
-#endif
+static uint32_t irq_interrupt_nesting;
+#define irq_isr_enter()                                                        \
+  int _irq_state = irq_disable();                                              \
+  irq_interrupt_nesting++;
 
-#define MAX_TIMERS TIMER_UNDEFINED
+/** Macro that has to be used at the exit point of an ISR */
+#define irq_isr_exit()                                                         \
+  if (irq_interrupt_nesting)                                                   \
+    irq_interrupt_nesting--;                                                   \
+  irq_restore(_irq_state);                                                     \
+  if (sched_context_switch_request)                                            \
+    thread_yield();
 
-typedef struct
-{
-    void (*cb)(void *, int);
+#define MAX_TIMERS TIMER_NUMOF
+
+typedef struct {
+  uint32_t conf;             // configuration
+  uint32_t timer_a_mode;     // timer A Mode
+  uint32_t timer_b_mode;     // timer B Mode
+  uint32_t ctrl;             // timer control register
+  uint8_t sync[8];           // sync
+  uint32_t intr_mask;        // interrupt mask
+  uint32_t intr_raw_stat;    // raw interrupt status
+  uint32_t masked_intr;      // masked interrupt
+  uint32_t intr_clear;       // interrupt clear
+  uint32_t interval_load_a;  // interval load a
+  uint32_t interval_load_b;  // interval load b
+  uint32_t match_a;          // timer match a
+  uint32_t match_b;          // timer match b
+  uint32_t prescale_a;       // timer prescale a
+  uint32_t prescale_b;       // timer prescale b
+  uint32_t prescale_match_a; // timer prescale match a
+  uint32_t prescale_match_b; // timer prescale match b
+  uint32_t timer_a;          // timer a
+  uint32_t timer_b;          // timer b
+  uint32_t val_a;            // timer value a
+  uint32_t val_b;            // timer value b
+  uint32_t rtc_predivide;    // RTC Predivide
+
+  // snapshots
+  uint32_t prescale_snaphot_a;  // timer prescale snapshot a
+  uint32_t prescale_snapshot_b; // timer prescale snapshot b
+  uint32_t val_snapshot_a;      // timer value snapshot a
+  uint32_t val_snapshot_b;      // timer value snapshot b
+  uint32_t dma_event;           // DMA event
+} cc3200_timer;
+
+typedef struct {
+  cc3200_timer *timer; // RIOT specific timer ID
+  bool init;           // indicates if timer is ready
+  int periph_id;       // TI id for the periph
+  uint8_t irqn;
+  timer_isr_ctx_t ctx;
 } timer_conf_t;
 
-static timer_conf_t config[MAX_TIMERS];
+static timer_conf_t timer_config[] = {{
+                                          .timer = (cc3200_timer *)TIMERA0_BASE,
+                                          .irqn = INT_TIMERA0A,
+                                          .periph_id = PRCM_TIMERA0,
+                                      },
+                                      {
+                                          .timer = (cc3200_timer *)TIMERA1_BASE,
+                                          .irqn = INT_TIMERA1A,
+                                          .periph_id = PRCM_TIMERA1,
+                                      },
+                                      {
+                                          .timer = (cc3200_timer *)TIMERA2_BASE,
+                                          .irqn = INT_TIMERA2A,
+                                          .periph_id = PRCM_TIMERA2,
 
-void irq_timer0_handler(void)
-{
-    timer_clear(TIMER_0, 0);
-    config[TIMER_0].cb(0, 0); // timer has one hw channel
-    if (sched_context_switch_request)
-    {
-        thread_yield();
-    }
+                                      },
+                                      {
+                                          .timer = (cc3200_timer *)TIMERA3_BASE,
+                                          .irqn = INT_TIMERA3A,
+                                          .periph_id = PRCM_TIMERA3,
+                                      }};
+
+void timerHandler(tim_t dev) {
+  irq_isr_enter();
+  timer_clear(dev, 0);
+  timer_config[dev].ctx.cb(timer_config[dev].ctx.arg,
+                           0); // timer has one hw channel
+  irq_isr_exit();
 }
 
-void irq_timer1_handler(void)
-{
-    timer_clear(TIMER_1, 0);
-    config[TIMER_1].cb(0, 0); // timer has one hw channel
-    if (sched_context_switch_request)
-    {
-        thread_yield();
-    }
+void irqTimer0Handler(void) { timerHandler(T0); }
+#ifdef T1
+void irqTimer1Handler(void) { timerHandler(T1); }
+#endif
+#ifdef T2
+void irqTimer2Handler(void) { timerHandler(T2); }
+#endif
+#ifdef T3
+void irqTimer3Handler(void) { timerHandler(T3); }
+#endif
+
+static inline void *getHandler(tim_t dev) {
+  switch (dev) {
+#ifdef T1
+  case T1:
+    return irqTimer1Handler;
+#endif
+#ifdef T2
+  case T2:
+    return irqTimer2Handler;
+#endif
+#ifdef T3
+  case T3:
+    return irqTimer3Handler;
+#endif
+  default:
+    return irqTimer0Handler;
+  };
 }
 
-void irq_timer2_handler(void)
-{
-    timer_clear(TIMER_2, 0);
-    config[TIMER_2].cb(0, 0); // timer has one hw channel
-    if (sched_context_switch_request)
-    {
-        thread_yield();
-    }
+int timer_init(tim_t dev, unsigned long freq, timer_cb_t cb, void *arg) {
+
+  // check if timer id is valid
+  if (dev >= MAX_TIMERS) {
+    return -1;
+  }
+  void *timerHandler = getHandler(dev);
+  if (timerHandler == NULL) {
+    return -1;
+  }
+
+  uint32_t base = (int)timer_config[dev].timer;
+
+  // enable periph clock
+  MAP_PRCMPeripheralClkEnable(timer_config[dev].periph_id, PRCM_RUN_MODE_CLK);
+
+  // reset timer
+  MAP_PRCMPeripheralReset(timer_config[dev].periph_id);
+
+  // setup timer
+  MAP_TimerConfigure(base, TIMER_CFG_PERIODIC_UP);
+  MAP_TimerControlStall(base, TIMER_A, true);
+
+  // register & setup intrrupt handling
+  MAP_TimerIntRegister(base, TIMER_A, timerHandler);
+  timer_config[dev].ctx.cb = cb;
+  MAP_IntPrioritySet(timer_config[dev].irqn, INT_PRIORITY_LVL_2);
+
+  // enable the timer
+  MAP_TimerEnable(base, TIMER_A);
+
+  return 0;
 }
 
-void irq_timer3_handler(void)
-{
-    timer_clear(TIMER_3, 0);
-    config[TIMER_3].cb(0, 0); // timer has one hw channel
-    if (sched_context_switch_request)
-    {
-        thread_yield();
-    }
+int set_absolute(tim_t dev, int channel, unsigned long long value) {
+  if (dev >= MAX_TIMERS) {
+    return -1;
+  }
+  MAP_TimerMatchSet((uint32_t)timer_config[dev].timer, TIMER_A, value);
+  timer_config[dev].timer->intr_mask |= TIMER_TIMA_MATCH;
+  return 0;
 }
 
-int timer_init(tim_t dev, unsigned long freq, timer_cb_t cb, void *arg)
-{
-    UNUSED(freq);
-    UNUSED(arg);
-    switch (dev)
-    {
-    case TIMER_0:
-        //
-        // Enable and Reset the timer block
-        //
-        MAP_PRCMPeripheralClkEnable(PRCM_TIMERA0, PRCM_RUN_MODE_CLK);
-        MAP_PRCMPeripheralReset(PRCM_TIMERA0);
+int timer_set(tim_t dev, int channel, unsigned int timeout) {
+  return set_absolute(dev, channel, timer_config[dev].timer->timer_a + timeout);
+}
 
-        MAP_TimerConfigure(TIMERA0_BASE, TIMER_CFG_PERIODIC_UP);
+int timer_set_absolute(tim_t dev, int channel, unsigned int value) {
+  return set_absolute(dev, channel, value);
+}
 
-        MAP_TimerControlStall(TIMERA0_BASE, TIMER_A, true);
-        // register the handler
-        MAP_TimerIntRegister(TIMERA0_BASE, TIMER_A, irq_timer0_handler);
+int timer_clear(tim_t dev, int channel) {
+  if (dev >= MAX_TIMERS) {
+    return -1;
+  }
+  MAP_TimerIntClear((uint32_t)timer_config[dev].timer, TIMER_TIMA_MATCH);
+  // disable the match timer
+  timer_config[dev].timer->intr_mask &= ~(TIMER_TIMA_MATCH);
+  return 0;
+}
 
-        config[TIMER_0].cb = cb;
-
-        MAP_IntPrioritySet(INT_TIMERA0A, INT_PRIORITY_LVL_2);
-
-        // enable the timer
-        MAP_TimerEnable(TIMERA0_BASE, TIMER_A);
-
-        break;
-    case TIMER_1:
-        //
-        // Enable and Reset the timer block
-        //
-        MAP_PRCMPeripheralClkEnable(PRCM_TIMERA1, PRCM_RUN_MODE_CLK);
-        MAP_PRCMPeripheralReset(PRCM_TIMERA1);
-
-        MAP_TimerConfigure(TIMERA1_BASE, TIMER_CFG_PERIODIC_UP);
-
-        MAP_TimerControlStall(TIMERA1_BASE, TIMER_A, true);
-
-        // register the handler
-        MAP_TimerIntRegister(TIMERA1_BASE, TIMER_A, irq_timer1_handler);
-
-        config[TIMER_1].cb = cb;
-
-        MAP_IntPrioritySet(INT_TIMERA1A, INT_PRIORITY_LVL_2);
-
-        // enable the timer
-        MAP_TimerEnable(TIMERA1_BASE, TIMER_A);
-
-        break;
-    case TIMER_2:
-        //
-        // Enable and Reset the timer block
-        //
-        MAP_PRCMPeripheralClkEnable(PRCM_TIMERA2, PRCM_RUN_MODE_CLK);
-        MAP_PRCMPeripheralReset(PRCM_TIMERA2);
-
-        MAP_TimerConfigure(TIMERA2_BASE, TIMER_CFG_PERIODIC_UP);
-
-        MAP_TimerControlStall(TIMERA2_BASE, TIMER_A, true);
-
-        // register the handler
-        MAP_TimerIntRegister(TIMERA2_BASE, TIMER_A, irq_timer2_handler);
-
-        config[TIMER_2].cb = cb;
-
-        MAP_IntPrioritySet(INT_TIMERA2A, INT_PRIORITY_LVL_2);
-
-        // enable the timer
-        MAP_TimerEnable(TIMERA2_BASE, TIMER_A);
-
-        break;
-    case TIMER_3:
-        //
-        // Enable and Reset the timer block
-        //
-        MAP_PRCMPeripheralClkEnable(PRCM_TIMERA3, PRCM_RUN_MODE_CLK);
-        MAP_PRCMPeripheralReset(PRCM_TIMERA3);
-
-        MAP_TimerConfigure(TIMERA3_BASE, TIMER_CFG_PERIODIC_UP);
-
-        MAP_TimerControlStall(TIMERA3_BASE, TIMER_A, true);
-
-        // register the handler
-        MAP_TimerIntRegister(TIMERA3_BASE, TIMER_A, irq_timer3_handler);
-
-        config[TIMER_3].cb = cb;
-
-        MAP_IntPrioritySet(INT_TIMERA3A, INT_PRIORITY_LVL_2);
-
-        // enable the timer
-        MAP_TimerEnable(TIMERA3_BASE, TIMER_A);
-
-        break;
-    default:
-        break;
-    }
-
+unsigned int timer_read(tim_t dev) {
+  if (dev >= MAX_TIMERS) {
     return 0;
+  }
+  return timer_config[dev].timer->val_a;
 }
 
-int set_absolute(tim_t dev, int channel, unsigned long long value)
-{
-    UNUSED(channel);
-    switch (dev)
-    {
-    case TIMER_0:
-        MAP_TimerMatchSet(TIMERA0_BASE, TIMER_A, value);
-        // enable the match timer
-        HWREG(TIMERA0_BASE + TIMER_O_IMR) |= TIMER_TIMA_MATCH;
-        break;
-    case TIMER_1:
-        MAP_TimerMatchSet(TIMERA1_BASE, TIMER_A, value);
-        // enable the match timer
-        HWREG(TIMERA1_BASE + TIMER_O_IMR) |= TIMER_TIMA_MATCH;
-        break;
-    case TIMER_2:
-        MAP_TimerMatchSet(TIMERA2_BASE, TIMER_A, value);
-        // enable the match timer
-        HWREG(TIMERA2_BASE + TIMER_O_IMR) |= TIMER_TIMA_MATCH;
-        break;
-    case TIMER_3:
-        MAP_TimerMatchSet(TIMERA3_BASE, TIMER_A, value);
-        // enable the match timer
-        HWREG(TIMERA3_BASE + TIMER_O_IMR) |= TIMER_TIMA_MATCH;
-        break;
-    default:
-        break;
-    }
-
-    return 0;
+void timer_start(tim_t dev) {
+  if (dev >= MAX_TIMERS) {
+    return;
+  }
+  MAP_TimerEnable((uint32_t)timer_config[dev].timer, TIMER_A);
 }
 
-int timer_set(tim_t dev, int channel, unsigned int timeout)
-{
-
-    switch (dev)
-    {
-    case TIMER_0:
-        return set_absolute(dev, channel,
-                            HWREG(TIMERA0_BASE + TIMER_O_TAR) + timeout);
-    case TIMER_1:
-        return set_absolute(dev, channel,
-                            HWREG(TIMERA1_BASE + TIMER_O_TAR) + timeout);
-    case TIMER_2:
-        return set_absolute(dev, channel,
-                            HWREG(TIMERA2_BASE + TIMER_O_TAR) + timeout);
-    case TIMER_3:
-        return set_absolute(dev, channel,
-                            HWREG(TIMERA3_BASE + TIMER_O_TAR) + timeout);
-    default:
-        break;
-    }
-
-    return 0;
+void timer_stop(tim_t dev) {
+  if (dev >= MAX_TIMERS) {
+    return;
+  }
+  MAP_TimerDisable((uint32_t)timer_config[dev].timer, TIMER_A);
 }
 
-int timer_set_absolute(tim_t dev, int channel, unsigned int value)
-{
-    return set_absolute(dev, channel, value);
+void timer_irq_enable(tim_t dev) {
+  if (dev >= MAX_TIMERS) {
+    return;
+  }
+  MAP_TimerIntEnable((uint32_t)timer_config[dev].timer, TIMER_TIMA_MATCH);
 }
 
-int timer_clear(tim_t dev, int channel)
-{
-    UNUSED(channel);
-    switch (dev)
-    {
-    case TIMER_0:
-        MAP_TimerIntClear(TIMERA0_BASE, TIMER_TIMA_MATCH);
-        // disable the match timer
-        HWREG(TIMERA0_BASE + TIMER_O_IMR) &= ~(TIMER_TIMA_MATCH);
-        break;
-    case TIMER_1:
-        MAP_TimerIntClear(TIMERA1_BASE, TIMER_TIMA_MATCH);
-        // disable the match timer
-        HWREG(TIMERA1_BASE + TIMER_O_IMR) &= ~(TIMER_TIMA_MATCH);
-        break;
-    case TIMER_2:
-        MAP_TimerIntClear(TIMERA2_BASE, TIMER_TIMA_MATCH);
-        // disable the match timer
-        HWREG(TIMERA2_BASE + TIMER_O_IMR) &= ~(TIMER_TIMA_MATCH);
-        break;
-    case TIMER_3:
-        MAP_TimerIntClear(TIMERA3_BASE, TIMER_TIMA_MATCH);
-        // disable the match timer
-        HWREG(TIMERA3_BASE + TIMER_O_IMR) &= ~(TIMER_TIMA_MATCH);
-        break;
-    default:
-        break;
-    }
-    return 0;
+void timer_irq_disable(tim_t dev) {
+  if (dev >= MAX_TIMERS) {
+    return;
+  }
+  MAP_TimerIntDisable((uint32_t)timer_config[dev].timer, TIMER_TIMA_MATCH);
 }
 
-unsigned int timer_read(tim_t dev)
-{
-    switch (dev)
-    {
-    case TIMER_0:
-        return MAP_TimerValueGet(TIMERA0_BASE, TIMER_A);
-        break;
-    case TIMER_1:
-        return MAP_TimerValueGet(TIMERA1_BASE, TIMER_A);
-        break;
-    case TIMER_2:
-        return MAP_TimerValueGet(TIMERA2_BASE, TIMER_A);
-        break;
-    case TIMER_3:
-        return MAP_TimerValueGet(TIMERA3_BASE, TIMER_A);
-        break;
-    default:
-        return 0;
-    }
-}
+void timer_reset(tim_t dev) {
+  if (dev >= MAX_TIMERS) {
+    return;
+  }
 
-void timer_start(tim_t dev)
-{
-    switch (dev)
-    {
-    case TIMER_0:
-        MAP_TimerEnable(TIMERA0_BASE, TIMER_A);
-        break;
-    case TIMER_1:
-        MAP_TimerEnable(TIMERA1_BASE, TIMER_A);
-        break;
-    case TIMER_2:
-        MAP_TimerEnable(TIMERA2_BASE, TIMER_A);
-        break;
-    case TIMER_3:
-        MAP_TimerEnable(TIMERA3_BASE, TIMER_A);
-        break;
-    default:
-        break;
-    }
-}
-
-void timer_stop(tim_t dev)
-{
-    switch (dev)
-    {
-    case TIMER_0:
-        MAP_TimerDisable(TIMERA0_BASE, TIMER_A);
-        break;
-    case TIMER_1:
-        MAP_TimerDisable(TIMERA1_BASE, TIMER_A);
-        break;
-    case TIMER_2:
-        MAP_TimerDisable(TIMERA2_BASE, TIMER_A);
-        break;
-    case TIMER_3:
-        MAP_TimerDisable(TIMERA3_BASE, TIMER_A);
-        break;
-    default:
-        break;
-    }
-}
-
-void timer_irq_enable(tim_t dev)
-{
-    switch (dev)
-    {
-    case TIMER_0:
-        MAP_TimerIntEnable(TIMERA0_BASE, TIMER_TIMA_MATCH);
-        break;
-    case TIMER_1:
-        MAP_TimerIntEnable(TIMERA1_BASE, TIMER_TIMA_MATCH);
-        break;
-    case TIMER_2:
-        MAP_TimerIntEnable(TIMERA2_BASE, TIMER_TIMA_MATCH);
-        break;
-    case TIMER_3:
-        MAP_TimerIntEnable(TIMERA3_BASE, TIMER_TIMA_MATCH);
-        break;
-    default:
-        break;
-    }
-}
-
-void timer_irq_disable(tim_t dev)
-{
-    switch (dev)
-    {
-    case TIMER_0:
-        MAP_TimerIntDisable(TIMERA0_BASE, TIMER_TIMA_MATCH);
-        break;
-    case TIMER_1:
-        MAP_TimerIntDisable(TIMERA1_BASE, TIMER_TIMA_MATCH);
-        break;
-    case TIMER_2:
-        MAP_TimerIntDisable(TIMERA2_BASE, TIMER_TIMA_MATCH);
-        break;
-    case TIMER_3:
-        MAP_TimerIntDisable(TIMERA3_BASE, TIMER_TIMA_MATCH);
-        break;
-    default:
-        break;
-    }
-}
-
-void timer_reset(tim_t dev)
-{
-    switch (dev)
-    {
-    case TIMER_0:
-        TimerValueSet(TIMERA0_BASE, TIMER_A, 0);
-        break;
-    case TIMER_1:
-        TimerValueSet(TIMERA1_BASE, TIMER_A, 0);
-        break;
-    case TIMER_2:
-        TimerValueSet(TIMERA2_BASE, TIMER_A, 0);
-        break;
-    case TIMER_3:
-        TimerValueSet(TIMERA3_BASE, TIMER_A, 0);
-        break;
-    default:
-        break;
-    }
+  // TODO: for now only timer a is supported
+  timer_config[dev].timer->val_a = 0;
 }
