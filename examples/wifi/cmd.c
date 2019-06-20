af9b53e155b63b6c4db40a9e9f39838fd1b5aa46
Wlad Meixner <wlad.meixner@protonmail.com>
2019-06-20 16:04:08 +0200

    cc3200: refactor communication protocol

--- examples/wifi/cmd.c
+++ examples/wifi/cmd.c
@@ -3,6 +3,7 @@
 #include "driver.h"
 #include "protocol.h"
 #include "setup.h"
+#include "state.h"
 #include "utils.h"
 #include "xtimer.h"
 #include <stdio.h>
@@ -12,10 +13,12 @@
 #define SL_DEVICE_GENERAL_CONFIGURATION_DATE_TIME (11)
 #define SL_DEVICE_GENERAL_VERSION (12)
 #define SL_DEVICE_STATUS (2)
+#define CMD_TIMEOUT 40
 
 typedef struct DriverMessage {
-  _SlGenericHeader_t *header; // specifies opcode & total command size
-  uint8_t *cmdDesc;           // command description
+  uint16_t Opcode;     // specifies opcode & total command size
+  uint16_t RespOpcode; // response opcode
+  uint8_t *cmdDesc;    // command description
   uint8_t
       cmdDescLen; // length of the command description (not protocol aligned)
   uint8_t *payload;
@@ -24,8 +27,8 @@ typedef struct DriverMessage {
 } DriverMessage;
 
 typedef struct DriverResponse {
-  uint8_t *cmdRespDesc;
-  uint8_t *payload;
+  uint16_t resLen; // full length of response respDesc + payload
+  uint8_t *data;   // full response data
 } DriverResponse;
 
 const _SlCmdCtrl_t _SlDeviceGetCmdCtrl = {SL_OPCODE_DEVICE_DEVICEGET,
@@ -34,27 +37,28 @@ const _SlCmdCtrl_t _SlDeviceGetCmdCtrl = {SL_OPCODE_DEVICE_DEVICEGET,
 
 static bool isFirstCmd = true;
 
-#define DEVICE_INFO_RX_LEN sizeof(_DeviceSetGet_t) + sizeof(SlVersionFull)
+#define DEVICE_INFO_RX_LEN                                                     \
+  sizeof(_DeviceSetGet_t) + sizeof(SlVersionFull) + 4 // for some reason the
 
-void sendCommand(DriverMessage *msg, DriverResponse *res) {}
-
-void getDeviceInfo(SlVersionFull *ver) {
-  uint8_t buf[DEVICE_INFO_RX_LEN];
-
-  uint8_t DeviceGetId = SL_DEVICE_GENERAL_CONFIGURATION;
+void sendCommand(DriverMessage *msg, DriverResponse *res) {
   sendShortSync();
-  uint16_t cmdDescLen = alignDataLen(sizeof(_DeviceSetGet_t));
-  sendHeader(SL_OPCODE_DEVICE_DEVICEGET, cmdDescLen);
 
-  // send command description
-  _DeviceSetGet_t cmdDesc;
-  cmdDesc.DeviceSetId = DeviceGetId;
-  cmdDesc.Option = SL_DEVICE_GENERAL_VERSION;
+  // compute the total message size (the header is ignored by the server)
+  uint16_t cmdDescLen = alignDataLen(msg->payloadLen + msg->cmdDescLen);
+  _SlGenericHeader_t header = {.Opcode = SL_OPCODE_DEVICE_DEVICEGET,
+                               .Len = cmdDescLen};
+  // add request to the request queue
+  DriverRequest drReq = {.ID = 2,
+                         .Opcode = msg->RespOpcode,
+                         .Waiting = true,
+                         .BufferSize = res->resLen,
+                         .Buffer = res->data};
 
-  // mask rx interrupt
-  maskWifiInterrupt();
+  sendHeader(&header);
+  addToQueue(&drReq);
 
-  send((uint8_t *)&cmdDesc, cmdDescLen);
+  // send command descriptions
+  send((uint8_t *)msg->cmdDesc, alignDataLen(msg->cmdDescLen));
 
   // give SPI some time to "stabilize" as done in TIs code
   if (isFirstCmd) {
@@ -65,22 +69,35 @@ void getDeviceInfo(SlVersionFull *ver) {
       ;
   }
 
-  cc3200_SlResponseHeader cmd;
-
-  while (1) {
-    readCmdHeader(&cmd);
-    printf("GOT RESPONSE: opcode=%d", cmd.GenHeader.Opcode);
-    if (cmd.GenHeader.Opcode != SL_OPCODE_DEVICE_DEVICEGETRESPONSE) {
-      // call default handler
-      defaultCommandHandler(&cmd);
-    } else {
-      break;
-    }
+  // wait for message response (rxHandler will copy the value to the res buffer)
+  // TODO: may lead to problems in multithreaded envs
+  for (int i = CMD_TIMEOUT; (drReq.Waiting == 1) && (i > 0); i--) {
+    xtimer_usleep(250);
   }
-  uint16_t rxPayloadLen = sizeof(SlVersionFull) + _SlDeviceGetCmdCtrl.RxDescLen;
-  // now that we have the response we waited for
-  read(buf, alignDataLen(rxPayloadLen));
-  unmaskWifiInterrupt();
+}
 
+void getDeviceInfo(SlVersionFull *ver) {
+  _DeviceSetGet_t cmdDesc;
+  cmdDesc.DeviceSetId = SL_DEVICE_GENERAL_CONFIGURATION;
+  cmdDesc.Option = SL_DEVICE_GENERAL_VERSION;
+
+  // create msg struct
+  DriverMessage msg = {.Opcode = SL_OPCODE_DEVICE_DEVICEGET,
+                       .RespOpcode = SL_OPCODE_DEVICE_DEVICEGETRESPONSE,
+                       .cmdDesc = (uint8_t *)&cmdDesc,
+                       .cmdDescLen = sizeof(_DeviceSetGet_t),
+                       .payloadLen = 0};
+
+  // prepare response
+  // uint16_t resLen = sizeof(SlVersionFull) + msg.cmdDescLen;
+  uint8_t buf[DEVICE_INFO_RX_LEN];
+  DriverResponse res = {
+      .resLen = DEVICE_INFO_RX_LEN,
+      .data = buf,
+  };
+
+  sendCommand(&msg, &res);
+
+  // copy version info back to user
   memcpy(ver, &buf[sizeof(_DeviceSetGet_t)], sizeof(SlVersionFull));
 }
