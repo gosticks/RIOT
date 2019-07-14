6a0d8ea6e9bf8ff51ea6c4703d7e835299f466de
Wlad Meixner <wlad.meixner@protonmail.com>
2019-07-14 14:00:03 +0200

    cc3200: pm cleanup

--- cpu/cc3200/periph/pm.c
+++ cpu/cc3200/periph/pm.c
@@ -6,33 +6,20 @@
  * directory for more details.
  */
 
-#include "driverlib/rom_map.h"
 #include "periph/pm.h"
+#include "driverlib/rom_map.h"
 
 #ifdef PROVIDES_PM_SET_LOWEST_CORTEXM
-// TODO: needs to be impemented 
-void pm_set_lowest(void)
-{
-    /* this will hibernate with no way to wake up for now */
-    
-    // write to the hibernate register
-    HWREG(HIB3P3_BASE+HIB3P3_O_MEM_HIB_REQ) = 0x1;
+// TODO: needs to be impemented
+void pm_set_lowest(void) {
+  /* this will hibernate with no way to wake up for now */
 
-    // wait for 200 uSec 
-    UtilsDelay((80*200)/3);
+  // write to the hibernate register
+  HWREG(HIB3P3_BASE + HIB3P3_O_MEM_HIB_REQ) = 0x1;
+
+  // wait for 200 uSec
+  UtilsDelay((80 * 200) / 3);
 }
 #endif
 
-void pm_off(void)
-{
-    /* No Generic Power off Mechanism */
-}
-
-// wait based on 3 cycles of the CPU
-void __attribute__((naked))
-delay(unsigned long ulCount)
-{
-    __asm("    subs    r0, #1\n"
-          "    bne     delay\n"
-          "    bx      lr");
-}
+void pm_off(void) { /* No Generic Power off Mechanism */ }
