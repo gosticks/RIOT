86d41b86eb540380b31ff928caf2971355122857
Wlad Meixner <wlad.meixner@protonmail.com>
2019-10-14 03:41:24 +0200

    test ICMP packege send

--- drivers/cc3100/cc3100_netdev.c
+++ drivers/cc3100/cc3100_netdev.c
@@ -30,6 +30,33 @@ static void _isr(netdev_t *netdev);
 static int _get(netdev_t *netdev, netopt_t opt, void *val, size_t max_len);
 static int _set(netdev_t *netdev, netopt_t opt, const void *val, size_t len);
 
+char RawData_Ping[] = {
+                   /*---- wlan header start -----*/
+                   0x88,                                /* version , type sub type */
+                   0x02,                                /* Frame control flag */
+                   0x2C, 0x00,
+                   0x00, 0x23, 0x75, 0x55,0x55, 0x55,   /* destination */
+                   0x00, 0x22, 0x75, 0x55,0x55, 0x55,   /* bssid */
+                   0x08, 0x00, 0x28, 0x19,0x02, 0x85,   /* source */
+                   0x80, 0x42, 0x00, 0x00,
+                   0xAA, 0xAA, 0x03, 0x00, 0x00, 0x00, 0x08, 0x00, /* LLC */
+                   /*---- ip header start -----*/
+                   0x45, 0x00, 0x00, 0x54, 0x96, 0xA1, 0x00, 0x00, 0x40, 0x01,
+                   0x57, 0xFA,                          /* checksum */
+                   0xc0, 0xa8, 0x01, 0x64,              /* src ip */
+                   0xc0, 0xa8, 0x01, 0x02,              /* dest ip  */
+                   /* payload - ping/icmp */
+                   0x08, 0x00, 0xA5, 0x51,
+                   0x5E, 0x18, 0x00, 0x00, 0x41, 0x08, 0xBB, 0x8D, 0x00, 0x00, 0x00, 0x00,
+                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+                    };
+
+
 static irq_event_t _irq_nwp_event = IRQ_EVENT_INIT;
 
 
@@ -107,6 +134,10 @@ static int _init(netdev_t *netdev)
     /* setup the nwp */
     _nwp_setup(dev);
 
+    while (true) {
+        _nwp_send_raw_frame(dev, dev->sock_id, &RawData_Ping, sizeof(RawData_Ping), SL_RAW_RF_TX_PARAMS(WIFI_CHANNEL, 1, 0, 0));
+    }
+
     ROM_IntRegister(INT_NWPIC, (void *)isr_nwp);
     NVIC_SetPriority(NWPIC_IRQn, 2);
     NVIC_ClearPendingIRQ(NWPIC_IRQn);
@@ -116,7 +147,7 @@ static int _init(netdev_t *netdev)
 
 static void _isr(netdev_t *netdev)
 {
-    puts("###ISR");
+    puts("ISR called");
     /* notify netdev of finished isr */
     netdev->event_callback(netdev, NETDEV_EVENT_RX_COMPLETE);
 }
@@ -124,7 +155,6 @@ static void _isr(netdev_t *netdev)
 static int _send(netdev_t *netdev, const iolist_t *iolist)
 {
     cc3100_t *dev = (cc3100_t *)netdev;
-        iolist->
     int pkt_len = 0;
     /* send items in a packet one after the other */
     // TODO: pack multiple packets into one packet
@@ -180,7 +210,6 @@ static int _set(netdev_t *netdev, netopt_t opt, const void *val, size_t val_len)
     }
     switch (opt) {
     case NETOPT_RX_END_IRQ:
-        return 1;
     case NETOPT_RX_START_IRQ:
     case NETOPT_TX_START_IRQ:
     case NETOPT_TX_END_IRQ:
