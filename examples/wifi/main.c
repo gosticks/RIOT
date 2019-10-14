784ca38c53d94141009ebee337137ff531ef935b
Wlad Meixner <wlad.meixner@protonmail.com>
2019-10-14 03:19:14 +0200

    expand example

--- examples/wifi/main.c
+++ examples/wifi/main.c
@@ -1,35 +1,22 @@
-/*
- * Copyright (C) 2014 Freie Universität Berlin
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
-
-/**
- * @ingroup     examples
- * @{
- *
- * @file
- * @brief       Hello World application
- *
- * @author      Kaspar Schleiser <kaspar@schleiser.de>
- * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
- *
- * @}
- */
 
 #include <stdio.h>
 #include <string.h>
-#define ENABLE_DEBUG (1)
-#include "debug.h"
-#include "periph/gpio.h"
-#include "xtimer.h"
 
 #include "vendor/rom.h"
 
+#include "xtimer.h"
+#include "net/gnrc.h"
+#include "net/gnrc/ipv6.h"
+#include "net/gnrc/netif.h"
+#include "net/gnrc/netif/hdr.h"
+#include "net/gnrc/udp.h"
+#include "net/gnrc/pktdump.h"
 #include "msg.h"
 #include "shell.h"
+#include "periph/gpio.h"
+
+#define ENABLE_DEBUG (1)
+#include "debug.h"
 
 int test(int argc, char **argv)
 {
@@ -45,15 +32,20 @@ static const shell_command_t shell_commands[] = { { "test", "test shell",
 /* Forward declarations */
 int16_t start_pairing(int16_t sd);
 
+extern void send(char *addr_str, char *port_str, char *data, unsigned int num,
+                 unsigned int delay);
+
 int main(void)
 {
     int16_t status = 0;
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
 
+    send("2001:0db8:85a3:0000:0000:8a2e:0370:7334", "8080", "TEST TEST TEST TEST ", 100, 10000);
+
     /* start shell */
-    printf("All up, running the shell now\n");
-    char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    // printf("All up, running the shell now\n");
+    // char line_buf[SHELL_DEFAULT_BUFSIZE];
+    // shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
 
     return 0;
 }
---
+++ examples/wifi/send.c
@@ -0,0 +1,99 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#include "vendor/rom.h"
+
+#include "xtimer.h"
+#include "net/gnrc.h"
+#include "net/gnrc/ipv6.h"
+#include "net/gnrc/netif.h"
+#include "net/gnrc/netif/hdr.h"
+#include "net/gnrc/udp.h"
+#include "net/gnrc/pktdump.h"
+#include "msg.h"
+#include "shell.h"
+#include "periph/gpio.h"
+
+#define ENABLE_DEBUG (1)
+#include "debug.h"
+
+void send(char *addr_str, char *port_str, char *data, unsigned int num,
+                 unsigned int delay)
+{
+    puts("WHAT WHAT");
+    gnrc_netif_t *netif;
+    int iface;
+    uint16_t port;
+    ipv6_addr_t addr;
+
+    /* get interface, if available */
+    iface = ipv6_addr_split_iface(addr_str);
+    if ((iface < 0) && (gnrc_netif_numof() == 1)) {
+        puts("ONLY ONE");
+        netif = gnrc_netif_iter(NULL);
+    }
+    else {
+        puts("MORE");
+        netif = gnrc_netif_get_by_pid(iface);
+    }
+    /* parse destination address */
+    if (ipv6_addr_from_str(&addr, addr_str) == NULL) {
+        puts("Error: unable to parse destination address");
+        return;
+    }
+    /* parse port */
+    port = atoi(port_str);
+    if (port == 0) {
+        puts("Error: unable to parse destination port");
+        return;
+    }
+
+    for (unsigned int i = 0; i < num; i++) {
+        gnrc_pktsnip_t *payload, *udp, *ip;
+        unsigned payload_size;
+        /* allocate payload */
+        payload = gnrc_pktbuf_add(NULL, data, strlen(data), GNRC_NETTYPE_UNDEF);
+        if (payload == NULL) {
+            puts("Error: unable to copy data to packet buffer");
+            return;
+        }
+        /* store size for output */
+        payload_size = (unsigned)payload->size;
+        /* allocate UDP header, set source port := destination port */
+        udp = gnrc_udp_hdr_build(payload, port, port);
+        if (udp == NULL) {
+            puts("Error: unable to allocate UDP header");
+            gnrc_pktbuf_release(payload);
+            return;
+        }
+        /* allocate IPv6 header */
+        ip = gnrc_ipv6_hdr_build(udp, NULL, &addr);
+        if (ip == NULL) {
+            puts("Error: unable to allocate IPv6 header");
+            gnrc_pktbuf_release(udp);
+            return;
+        }
+        /* add netif header, if interface was given */
+        if (netif != NULL) {
+
+            puts("BUILD PACKET");
+            gnrc_pktsnip_t *netif_hdr = gnrc_netif_hdr_build(NULL, 0, NULL, 0);
+
+            gnrc_netif_hdr_set_netif(netif_hdr->data, netif);
+            LL_PREPEND(ip, netif_hdr);
+        }
+        /* send packet */
+        if (!gnrc_netapi_dispatch_send(GNRC_NETTYPE_UDP, GNRC_NETREG_DEMUX_CTX_ALL, ip)) {
+            puts("Error: unable to locate UDP thread");
+            gnrc_pktbuf_release(ip);
+            return;
+        }
+        puts("PACKET SEND");
+        /* access to `payload` was implicitly given up with the send operation above
+         * => use temporary variable for output */
+        printf("Success: sent %u byte(s) to [%s]:%u\n", payload_size, addr_str,
+               port);
+        xtimer_usleep(delay);
+    }
+}
